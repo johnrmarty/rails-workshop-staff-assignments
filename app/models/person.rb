c5d0bcdd19b6feb886fbdac8a67cb4c97e67e096
John Marty <JohnRmarty@gmail.com>
2015-02-26 10:34:11 -0700

    validation for first, last, and title in person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,9 @@
 class Person < ActiveRecord::Base
 
+	validates :first_name, presence: true, uniqueness: {case_sensitive: false}
+	validates :last_name, presence: true, uniqueness: {case_sensitive: false}
+	validates :title, presence: true, uniqueness: {case_sensitive: false}
+
 	def full_name
   [first_name, last_name].join(' ')
 end
